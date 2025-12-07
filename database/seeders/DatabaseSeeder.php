f775af8d3717301f5fd38c1e1c99efb61686e984
Santiago Betancur <santi.betancur578@gmail.com>
2025-12-06 20:58:49 -0500

    Agrego AdminUserSeeder y limpio DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,17 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Ejemplo de usuario de prueba
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
         // Aquí llamamos a todos los seeders que queremos ejecutar
         $this->call([
             CategorySeeder::class,
             ProductSeeder::class,
-            AdminUserSeeder::class, // 👈 Añadido aquí
+            AdminUserSeeder::class, // 👈 añadimos el seeder del admin
         ]);
     }
 }
